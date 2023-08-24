361f582c786b01acf299ff5bf71d2efa93942000
DIΣGӨ L <di3golabrin@gmail.com>
2023-08-24 16:26:07 -0400

    modificación metodos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+    public void main(String[] args) {
         String[][] mazo = crearMatrizCartas();
         inicializarCartasJuego(mazo);
         for (int i = 0; i < mazo.length; i++) {
