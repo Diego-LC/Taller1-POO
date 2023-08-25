c4e386bb76fb518111bd921e64f3dfbbf65b4735
DIΣGӨ L <di3golabrin@gmail.com>
2023-08-25 16:58:20 -0400

    modificación métodos: implementado obtenerCartas()

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         String[][] mazo = crearMatrizCartas();
@@ -10,7 +11,7 @@ public class Main {
 
     // Método llamado crearMatrizCartas() que permita crear una matriz de cartas que indique su nombre y puntaje. Considere el largo de la matriz de como máximo 12 cartas.
     public static String[][] crearMatrizCartas(){
-        return new String[48][2];
+        return new String[12][2];
     }
     //método llamado agregarCartas(String nombreCarta, String puntaje) que permita agregar una carta a la matriz.
     public static void agregarCartas(String [][] matrizCartas, String nombreCarta, int puntaje){
@@ -25,30 +26,26 @@ public class Main {
 
     //método llamado inicializarCartasJuego() que permita agregar las cartas
     public static void inicializarCartasJuego(String [][] mazo){
-        String [] color = {"Corazones", "Diamantes", "Picas", "Tréboles"};
-        for (int i = 0; i < color.length; i++) {
-            for (int j = 0; j < 12; j++) {
-
-                if (j == 0){
-                    agregarCartas(mazo, "As de " + color[i], 11);
-                }
-                else if (j == 9){
-                    agregarCartas(mazo,"J de " + color[i], 10);
-                }
-                else if (j == 10){
-                    agregarCartas(mazo,"Q de " + color[i], 10);
-                }
-                else if (j == 11){
-                    agregarCartas(mazo,"K de " + color[i], 10);
-                }
-                else{
-                    agregarCartas(mazo, j+1 + " de " + color[i], j+1);
-                }
-            }
-        }
+        agregarCartas(mazo, "As de diamantes", 11);
+        agregarCartas(mazo, "Dos de tréboles", 2);
+        agregarCartas(mazo, "Tres de diamantes", 3);
+        agregarCartas(mazo, "Cuatro de picas", 4);
+        agregarCartas(mazo, "Cinco de corazones", 5);
+        agregarCartas(mazo, "Seis de tréboles", 6);
+        agregarCartas(mazo, "Siete de diamantes", 7);
+        agregarCartas(mazo, "Ocho de picas", 8);
+        agregarCartas(mazo, "Nueve de corazones", 9);
+        agregarCartas(mazo, "J de tréboles", 10);
+        agregarCartas(mazo, "Q de corazones", 10);
+        agregarCartas(mazo, "K de picas", 10);
     }
     
     //método llamado obtenerCartas() que permita a un jugador obtener una carta aleatoria.
+    public static String[] obtenerCartas(String [][] mazo){
+        Random random = new Random();
+        int indice = random.nextInt(mazo.length);
+        return mazo[indice];
+    }
     //método llamado jugar() que permita que ambos jugadores saquen sus cartas y valide quien es el ganador.
     //serie de métodos auxiliares que permita jugar varias veces hasta que el usuario quiera no seguir jugando a través de un menú.
     public static void mostrar_menu() {
